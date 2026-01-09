0bba9e4664e00400714e308d1b7155f84f5605ca
Tiago Lopes <tiago.lopes.comercial@gmail.com>
2026-01-09 20:54:29 -0300

    Adicionado arquivo de script para rodar a automacao via agendador de tarefas

---
+++ script.bat
@@ -0,0 +1,14 @@
+@echo off
+
+echo ==========================================
+echo    INICIANDO ROBO DE AJUSTE DE PONTO
+echo ==========================================
+
+echo [1/3] Ativando ambiente virtual...
+call .\venv\Scripts\activate
+
+echo [2/3] Executando codigo python
+python src/main.py
+
+echo [3/3] Processo finalizado com sucesso!
+echo ==========================================
