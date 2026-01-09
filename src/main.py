8e43f16dbe9446052a3421a4f106f3c24c983213
Tiago Lopes <tiago.lopes.comercial@gmail.com>
2026-01-09 14:50:14 -0300

    Adicionado fluxo de execução na main

--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ from datetime import date
 
 def main() -> None:
     logger.info('Iniciando automação')
+    
     today = date.today()
     today = date.strftime(today, '%d/%m/%Y')
     lista_dias_ajustar = [today]
     horarios_ponto = ['09:00', '12:00', '13:00', '18:00']
     
     ajustar_ponto(lista_dias_ajustar, horarios_ponto)
+
     logger.info('Automação finalizada com sucesso')
 
 if __name__ == '__main__':
