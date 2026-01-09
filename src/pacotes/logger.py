88274d650569c6e1c4280a8598894712e6afa96c
Tiago Lopes <tiago.lopes.comercial@gmail.com>
2026-01-09 14:48:52 -0300

    Adicionada funcao de logs

--- src/pacotes/logger.py
+++ src/pacotes/logger.py
@@ -2,10 +2,16 @@ import logging
 import os
 from pathlib import Path
 from datetime import date
-PASTA_LOGS = Path(os.getcwd(), 'logs')
+PASTA_LOGS = Path(os.getcwd(), 'src', 'logs')
 now = date.today()
 now = date.strftime(now, '%d-%m-%Y')
 caminho_log = PASTA_LOGS / f'log {now}.log'
+arquivo = Path.exists(caminho_log)
+if not arquivo:
+    with open(caminho_log, 'w', encoding='utf-8') as log:
+        log.close()
+
+
 logger = logging.getLogger("RPA")
 
 logger.setLevel(logging.INFO)
