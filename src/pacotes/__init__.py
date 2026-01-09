425d79ddcc53b8204f72af5c9c95e95d4f513998
Tiago Lopes <tiago.lopes.comercial@gmail.com>
2026-01-09 14:48:34 -0300

    Atualizado para reconhecer a funcao de validar cpf

--- src/pacotes/__init__.py
+++ src/pacotes/__init__.py
@@ -1,5 +1,5 @@
-from ajustar_ponto import ajustar_ponto
-from logger import logger
-from validar_cpf import validar_e_formatar_cpf
+from .validar_cpf import validar_e_formatar_cpf
+from .logger import logger
+from .ajustar_ponto import ajustar_ponto
 
 __all__ = ["ajustar_ponto", "logger", "validar_e_formatar_cpf"]
